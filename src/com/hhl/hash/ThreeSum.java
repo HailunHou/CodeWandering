f1b07a2f33a00cbd25caa5f22baf1b1d1de5e2c0
HailunHou <1635928471@qq.com>
2022-08-27 17:07:08 +0800

    Merge branch 'master' of https://github.com/HailunHou/CodeWandering

---
+++ src/com/hhl/hash/ThreeSum.java
@@ -0,0 +1,19 @@
+/**
+ * @author BOOM
+ * @create 2022-08-27 16:56
+ */
+package com.hhl.hash;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ThreeSum {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> list = new ArrayList<List<Integer>>();
+        for (int i = 0; i < nums.length; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
+
+        }
+    }
+}
