8c87472e077d6fb87ec95bd504e121db40102e50
HailunHou <1635928471@qq.com>
2023-04-12 19:21:16 +0800

    Merge branch 'master' of https://github.com/HailunHou/CodeWandering

--- src/com/hhl/test/TestArea.java
+++ src/com/hhl/test/TestArea.java
@@ -4,9 +4,11 @@
  */
 package com.hhl.test;
 
+import com.hhl.hash.Intersection;
 import org.junit.Test;
 
 import java.net.Inet4Address;
+import java.time.Year;
 import java.util.*;
 import java.util.stream.IntStream;
 
@@ -15,36 +17,6 @@ import java.util.stream.IntStream;
  @create 2023-04-04 21:49
  */
 
-class Solution {
-    public int strStr(String haystack, String needle) {
-        int n = haystack.length(), m = needle.length();
-        if (m == 0) {
-            return 0;
-        }
-        int[] pi = new int[m];
-        for (int i = 1, j = 0; i < m; i++) {
-            while (j > 0 && needle.charAt(i) != needle.charAt(j)) {
-                j = pi[j - 1];
-            }
-            if (needle.charAt(i) == needle.charAt(j)) {
-                j++;
-            }
-            pi[i] = j;
-        }
-        for (int i = 0, j = 0; i < n; i++) {
-            while (j > 0 && haystack.charAt(i) != needle.charAt(j)) {
-                j = pi[j - 1];
-            }
-            if (haystack.charAt(i) == needle.charAt(j)) {
-                j++;
-            }
-            if (j == m) {
-                return i - m + 1;
-            }
-        }
-        return -1;
-    }
-}
 
 
 public class TestArea {
@@ -119,12 +91,11 @@ public class TestArea {
 
     @Test
     public void test8(){
-        Solution solution = new Solution();
-        String ss = "aabaaaaaf";
-        String pp = "aaaaaf";
-        solution.strStr(ss, pp);
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+        System.out.println("queue.poll() = " + queue.poll());
     }
-
-
 
 }
