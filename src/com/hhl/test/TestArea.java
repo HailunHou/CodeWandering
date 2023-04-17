d6c8e0166773735a2ba62bd1b259caf256242df9
hhl <1635928471@qq.com>
2023-04-17 12:05:59 +0800

    Dorms Commit

--- src/com/hhl/test/TestArea.java
+++ src/com/hhl/test/TestArea.java
@@ -1,101 +1,64 @@
-/**
- * @author BOOM
- * @create 2023-04-04 21:49
- */
 package com.hhl.test;
 
-import com.hhl.hash.Intersection;
 import org.junit.Test;
 
-import java.net.Inet4Address;
-import java.time.Year;
 import java.util.*;
-import java.util.stream.IntStream;
 
-/**
- @author boom
- @create 2023-04-04 21:49
- */
+class Solution {
+    public String removeDuplicates(String s) {
+         Stack<Character> stack = new Stack<>();
+//        Queue<Character> queue = new ArrayDeque<>();
+        for(int i=0; i<s.length(); i++){
+            if(!stack.empty()){
+                if(stack.peek() == s.charAt(i)){
+                    stack.pop();
+                    continue;
+                }
+            }
+            stack.push(s.charAt(i));
+        }
+        StringBuilder ss = new StringBuilder();
 
-
-
+        StringBuffer sb = new StringBuffer();
+        while(!stack.empty()){
+            sb.append(stack.pop());
+        }
+        return new String(sb.reverse());
+    }
+}
 public class TestArea {
-
     @Test
     public void test1(){
-        System.out.println("(int)'a' = " + (int) 'a');
+
     }
 
     @Test
     public void test2(){
-        HashMap<Integer,Integer> numMap = new HashMap<>();
-        numMap.put(5, 1);
-        System.out.println(numMap);
-        numMap.put(5, 1);
-        System.out.println(numMap);
+        String a = "123";
+        char[] chars = a.toCharArray();
+        String s = new String(chars);
+//        System.out.println(s.equals(a));
+        System.out.println("s = " + s);
+        System.out.println("a = " + a);
     }
 
     @Test
     public void test3(){
-        HashMap<Integer,Integer> numMap = new HashMap<>();
-        numMap.put(5, 1);
-        System.out.println(numMap);
-        Integer integer = numMap.get(5);
-        System.out.println(integer.intValue());
+        Solution solution = new Solution();
+        String abbaca = solution.removeDuplicates("abbaca");
+        System.out.println(abbaca);
     }
 
     @Test
     public void test4(){
-        Set<Integer> numsSet = new HashSet<>();
-        numsSet.add(10);
-        System.out.println("numsSet.contains(10) = " + numsSet.contains(10));
-
+        Stack<String> stack = new Stack<>();
+        char c = 'a';
+        stack.push(new String(c+""));
+        System.out.println(new String(""+c));
+        String a=null;
+        
     }
 
-    @Test
-    public void test5(){
-        int[] nums = new int[]{2,7,11,15};
-        int target = 9;
-        System.out.println("twoSum(nums, target) = " + twoSum(nums, target));
-    }
 
-    public int[] twoSum(int nums[], int target){
-        HashMap<Integer, Integer> numsMap = new HashMap<>();
-        for(int i=0; i<nums.length; i++){
-            if(numsMap.containsKey(target-nums[i])){
-                return new int[]{i, numsMap.get(target-nums[i]).intValue()};
-            }
-            numsMap.put(nums[i], i);
-        }
-        return nums;
-    }
-
-    @Test
-    public void test6(){
-        int nums[] = new int[]{1,4,33,6,2};
-        IntStream sorted = Arrays.stream(nums).sorted();
-        int[] ints = sorted.toArray();
-        for (int anInt : ints) {
-            System.out.println(anInt);
-        }
-    }
-
-    @Test
-    public void test7(){
-        String s = "  hello world  ";
-        StringBuilder sb = new StringBuilder(s);
-        sb.setCharAt(0, 'a');
-        s = sb.toString();
-        System.out.println(s);
-    }
-
-    @Test
-    public void test8(){
-        Queue<Integer> queue = new ArrayDeque<>();
-        queue.add(1);
-        queue.add(2);
-        queue.add(3);
-        System.out.println("queue.poll() = " + queue.poll());
-    }
 
 }
